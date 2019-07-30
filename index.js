f82214b122ab46858dadd9850649de41a52efa60
Natalia Bandeira <nataliapbandeira@gmail.com>
2019-07-30 01:02:41 -0300

    Adding config files, environment files, and adjust api to genre api

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-const express = require('express');
 const Joi = require('joi');
-const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
 const mongoose = require('mongoose');
-const _ = require('underscore');
+const logger = require('./logger');
+const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
 
+app.use(logger);
+
+console.log(`Application name ${config.get('name')}`);
+console.log(`Mail Server ${config.get('mail.host')}`);
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan engabled...');
+}
 // mongoose.connect('')
 // server.use(bodyParser.json());
 // server.use(bodyParser.urlencoded({
 //     extended: true
 // }));
 // 
-// var cats = require('./cats.js')(app)
 
-const courses = [
-    { id: 1, course: "course1" },
-    { id: 2, course: "course2" },
-    { id: 3, course: "course3" }
+const genres = [
+    { id: 1, genre: "Pop" },
+    { id: 2, genre: "Folk" },
+    { id: 3, genre: "Rock" },
+    { id: 4, genre: "Country" },
+    { id: 5, genre: "Samba" },
+    { id: 6, genre: "Classical" },
+    { id: 7, genre: "R&B" }
 ];
 
 app.get('/', (req, res) => {
@@ -29,37 +46,71 @@ app.get('/about', (req, res) => {
     res.send('About page');
 });
 
-app.get('/api/courses/', (req, res) => {
-    res.send(courses);
+app.get('/api/genres/', (req, res) => {
+    res.send(genres);
 });
 
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) {
-        res.status(404).send("Id does not exist on array. ");
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) {
+        return res.status(404).send("Genre is not on the list.");
     }
-    res.send(course);
+    res.send(genre);
 });
 
-app.post('/api/courses', (req, res) => {
+app.post('/api/genres', (req, res) => {
+
+    const { error } = validategenre(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const genre = {
+        id: genres.length + 1,
+        genre: req.body.genre
+    }
+
+    genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) {
+        return res.status(404).send("Genre is not on the list.");
+    }
+
+    const { error } = validategenre(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    genre.genre = req.body.genre;
+
+    res.send(genre);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) {
+        return res.status(404).send("Genre is not on the list.");
+    }
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
+function validategenre(genre) {
     const schema = {
-        // name = Joi.string().min(3).required()
+        genre: Joi.string().min(3).required()
     };
 
-    const result = Joi.validate(req.body, schema);
-
-    if (result.error) {
-        res.status(400).send(result.error);
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-
-    courses.push(course);
-    res.send(course);
-});
+    return Joi.validate(genre, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
